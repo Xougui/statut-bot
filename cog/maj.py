a7d56a4feed0814f47700ebd4f19d5304a099ac2
Xougui <xougui.7@gmail.com>
2025-12-15 21:12:06 +0100

    Update maj.py

--- cog/maj.py
+++ cog/maj.py
@@ -16,9 +16,6 @@ from google.genai import types
 import PARAM
 
 # Configurez le logging
-logging.basicConfig(
-    level=logging.INFO, format="%(asctime)s - %(levelname)s - %(message)s"
-)
 
 load_dotenv()
 
